df173886688d0e805ebdd9521aeb8e648b74e6f2
adityabhosle-1 <68443991+adityabhosle-1@users.noreply.github.com>
2021-05-10 15:09:31 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,127 @@
+// Creating an array and passing the number, questions, options and answers
+
+let questions = [
+   {
+     numb: 1,
+     question: "What is Harry's middle name?",
+     answer: "James",
+     options: [
+         "Albus",
+         "James",
+         "Peter",
+         "Tom"
+     ]
+
+   },
+   {
+     numb: 2,
+     question: "Who does Hermione go to the yule ball with?",
+     answer: "Viktor Krum",
+     options: [
+         "Henry",
+         "Ron",
+         "Viktor Krum",
+         "Seamus"
+     ]
+
+   },
+   {
+     numb: 3,
+     question: "What house is Ginny in?",
+     answer: "Gryffindor",
+     options: [
+         "Slytherin",
+         "Ravenclaw",
+         "Hufflepuff",
+         "Gryffindor"
+     ]
+
+   },
+   {
+     numb: 4,
+     question: "What is the name of Voldemort's pet snake?",
+     answer: "Nagini",
+     options: [
+         "Buck beak",
+         "Nagini",
+         "Hogsmeads",
+         "Lily"
+     ]
+
+   },
+   {
+     numb: 5,
+     question: "What spell did Harry use to kill Lord Voldemort?",
+     answer: "Avada Kedavra",
+     options: [
+         "Accio",
+         "Expedo Patronum",
+         "Expelliarmus",
+         "Avada Kedavra"
+     ]
+
+   },
+   {
+     numb: 6,
+     question: "The 'Felifors' spell turns a cat into?",
+     answer: "Cauldron",
+     options: [
+         "Hat",
+         "Bat",
+         "Cauldron",
+         "Matchbox"
+     ]
+
+   },
+   {
+     numb: 7,
+     question: "Which patronus belongs to Luna Lovegood?",
+     answer: "Rabbit",
+     options: [
+         "Doe",
+         "Rabbit",
+         "Dog",
+         "Horse"
+     ]
+
+   },
+   {
+     numb: 8,
+     question: "Which element is associated with Hufflepuff?",
+     answer: "Earth",
+     options: [
+         "Fire",
+         "Air",
+         "Earth",
+         "Water"
+     ]
+
+   },
+   {
+     numb: 9,
+     question: "What was the name of the Black family's house elf?",
+     answer: "Kreacher",
+     options: [
+         "Dobby",
+         "Winky",
+         "Kreacher",
+         "Hokey"
+     ]
+
+   },
+   {
+     numb: 10,
+     question: "What position does Harry Potter play in Quidditch?",
+     answer: "Seeker",
+     options: [
+         "Chaser",
+         "Keeper",
+         "Bludger",
+         "Seeker"
+     ]
+
+   }
+
+
+
+];
